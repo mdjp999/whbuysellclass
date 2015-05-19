7b20d79d88863e27f543c4e93d56815f58a2e2b2
sy_work <mdjp@qq.com>
2015-05-19 16:13:06 +0800

    Signed-off-by: sy_work <mdjp@qq.com>

--- res/js/mt4tool.js
+++ res/js/mt4tool.js
@@ -1,6 +1,8 @@
 $(function () {
     //下单速度测试
     clickAndAlertResult("orderSpeedTestBtn", "../m4/orderSpeed");
+    //平掉所有
+    clickAndAlertResult("closeAllOrderBtn", "../m4/closeAllOrder");
 
 });
 
