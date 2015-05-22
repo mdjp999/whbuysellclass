7339320e5a9e9ee84d0688d366174d60019fda98
sy_work <mdjp@qq.com>
2015-05-22 19:19:01 +0800

    Signed-off-by: sy_work <mdjp@qq.com>

--- res/js/mt4tool.js
+++ res/js/mt4tool.js
@@ -3,11 +3,13 @@ $(function () {
     clickAndAlertResult("orderSpeedTestBtn", "../m4/orderSpeed");
     //平掉所有
     clickAndAlertResult("closeAllOrderBtn", "../m4/closeAllOrder");
+    //测试反向对冲平单
+    clickAndAlertResult("orderCloseByTestBtn", "../m4/ifSupportOrderCloseBy");
 
 });
 
-function clickAndAlertResult(id,url) {
-    $("#"+id).click(function () {
+function clickAndAlertResult(id, url) {
+    $("#" + id).click(function () {
         $.getJSON(url, function (r) {
             alert(r);
         });
